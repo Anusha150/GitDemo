037600ee2985ca3f61f2a1271a5b091f6790106a
anusha <ani.anushaj999@gmail>
2024-10-20 19:35:34 +0530

    new message

---
+++ src/test/java/rahulshettyacademy/ExtentReports/temp.java
@@ -0,0 +1,12 @@
+package rahulshettyacademy.ExtentReports;
+
+public class temp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public void branchCheck() {
+		System.out.println("Branchiesss");
+	}
+}
